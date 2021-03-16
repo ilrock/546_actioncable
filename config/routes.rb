43f49ec3f9ed45934302749648a4e3cce70efb88
Andrea Rocca <andrea.rocca3@gmail.com>
2021-03-16 08:14:33 +0100

    Lecture

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  
+  # /chatrooms/:id
+  resources :chatrooms, only: :show do
+    # POST /chatrooms/:chatroom_id/messages
+    resources :messages, only: :create
+  end
+  # DELETE /messages/:id
+  # resources :messages, only: :destroy
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
